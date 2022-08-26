e6e3547fe6cbb14a44b5c2650902a0a4c181123f
y8123y2fhwkg21 <112170653+y8123y2fhwkg21@users.noreply.github.com>
2022-08-26 18:52:36 +0200

    create github pages

--- assets/witsec-search/search.js
+++ assets/witsec-search/search.js
@@ -1,6 +1,7 @@
 let wsSearchDB = [];
 let wsScoreDB = [];
 let wsQ = null;
+let wsQinternal = null;
 
 // Display the results to the page
 function wsDisplayResults() {
@@ -29,16 +30,9 @@ function wsDisplayResults() {
 			.replace(/{searchString}/g, wsQ)
 			.replace(/{searchTotal}/g, wsScoreDB.length);
 
-		// Max character count for search result body
-		const searchMaxChars = (section.dataset.wssearchbodymaxchars !== undefined && !isNaN(section.dataset.wssearchbodymaxchars) ? section.dataset.wssearchbodymaxchars : "9999");
-
-		// Check if search string needs to be highlighted
-		let q = wsStripShortWords(wsQ);
-		q = q.replace(/ /g, "|"); // Prepare the regex, so it searches for all words in the search string
-		const regex =  new RegExp(q, "gi");
-		const searchHighlight = (section.dataset.wssearchhighlight === "true" && q.length > 0 ? true : false);
-		const searchHighlightColor = section.dataset.wssearchhighlightcolor;
-		const searchHighlightMarkup = "<mark style='background-color:" + searchHighlightColor + "; padding:0'>$&</mark>";
+		// Prepare search string highlighting
+		const regex =  new RegExp(wsQinternal, "gi");
+		const searchHighlightMarkup = "<mark>$&</mark>";
 
 		// Grab search result template
 		const template = section.querySelector(".wsSearchResultTemplate").outerHTML;
@@ -50,19 +44,12 @@ function wsDisplayResults() {
 
 		// Let's show the results now
 		wsScoreDB.forEach((entry, index) => {
-			// Check if we need to cutoff the body text
-			if (entry.body.length > searchMaxChars) {
-				entry.body = entry.body.substring(0, searchMaxChars);
-				while (entry.body[entry.body.length-1] === ".")	// Remove trailing dots
-					entry.body = entry.body.slice(0,-1);
-				entry.body = entry.body.trim() + "...";
-			}
+			// Cut off text from the body, if necessary
+			entry = wsCutOffText(entry);
 
-			// Check if we need to highlight the search results
-			if (searchHighlight) {			
-				entry.header = entry.header.replace(regex, searchHighlightMarkup);
-				entry.body = entry.body.replace(regex, searchHighlightMarkup);
-			}
+			// Always mark the search result, but the class will determine what it'll look like
+			entry.header = entry.header.replace(regex, searchHighlightMarkup);
+			entry.body = entry.body.replace(regex, searchHighlightMarkup);
 
 			// Replace all 'variables' with the value of this index
 			let res = template
@@ -86,13 +73,16 @@ function wsDisplayResults() {
 function wsPerformSearch() {
 	try {
 		// Don't search for words that are too short
-		let q = wsStripShortWords(wsQ);
+		wsQinternal = wsStripShortWords(wsQ);
+
+		// Handle diacritics
+		wsQinternal = wsHandleDiacritics(wsQinternal);
 
 		// If nothing's left after stripping, we shouldn't perform a search
-		if (q.length !== 0) {
+		if (wsQinternal.length !== 0) {
 			// Prepare the regex, so it searches for all words in the search string
-			q = q.replace(/ /g, "|");
-			const regex =  new RegExp(q, "gi");
+			wsQinternal = wsQinternal.replace(/ /g, "|");
+			const regex =  new RegExp(wsQinternal, "gi");
 
 			// If a match is found in the header, it's worth more than if a match is found in the body
 			const scoreHeader = 3;
@@ -106,6 +96,8 @@ function wsPerformSearch() {
 				// If there's a match, save it
 				if (countHeader > 0 || countBody > 0) {
 					entry.score = countHeader * scoreHeader + countBody * scoreBody;
+					const bodyPos = regex.exec(entry.body);
+					entry.bodyPos = (bodyPos ? bodyPos.index : 0);
 					wsScoreDB.push(entry);
 				}
 			});
@@ -139,7 +131,7 @@ function wsStripShortWords(q) {
 // Read the online "database"
 function wsReadDatabase() {
 	let xhr = new XMLHttpRequest();
-	let url = "assets/witsec-search/search.db" + "?" + Date.now();
+	let url = "assets/witsec-search/search.json" + "?" + Date.now();
 	xhr.open("GET", url, true);
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4) {
@@ -185,13 +177,128 @@ function wsSearchShowError(err) {
 	document.querySelector(".witsec-search .wsSearchError").classList.remove("d-none");
 }
 
+// Remove diacritics, then add variants of letters, so it doesn't matter if you're using diacritics in your search string or not - it'll search for anything
+function wsHandleDiacritics(str) {
+	const dc = {
+		"a": ["à", "á", "â", "ã", "ä", "å", "ā", "ă", "ą"],
+		"c": ["ç", "ć", "ĉ", "ċ", "č"],
+		"d": ["ď"],
+		"e": ["è", "é", "ê", "ë", "ē", "ĕ", "ė", "ę", "ě"],
+		"g": ["ĝ", "ğ", "ġ", "ģ"],
+		"h": ["ĥ"],
+		"i": ["ì", "í", "î", "ï", "ĩ", "ī", "ĭ", "į"],
+		"j": ["ĵ"],
+		"k": ["ķ"],
+		"l": ["ĺ", "ļ", "ľ"],
+		"n": ["ñ", "ń", "ņ", "ň"],
+		"o": ["ò", "ó", "ô", "ö", "õ", "ō", "ŏ", "ő"],
+		"r": ["ŕ", "ŗ", "ř"],
+		"s": ["ś", "ŝ", "ş", "š"],
+		"t": ["ţ", "ť"],
+		"u": ["ù", "ú", "û", "ü", "ũ", "ů", "ū", "ŭ", "ű", "ų"],
+		"w": ["ŵ"],
+		"y": ["ý", "ÿ", "ŷ"],
+		"z": ["ź", "ż", "ž"]
+	};
+
+	// Replace any existing diacritics with their 'normal' counterpart
+	str = str.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+	// Turn "lorem" into "l[o|ò|ó|ô|ö|õ|ō|ŏ|ő]rem". Not the most fancy way, but considering this runs on a client, it's fine
+	for (const key in dc) {
+		const r = "[" + key + "|" + dc[key].join("|") + "]";
+		const regex = new RegExp(key, "gi");
+		str = str.replace(regex, r);
+	}
+
+	return str;
+}
+
+// Cut off text if necessary
+function wsCutOffText(entry) {
+	const section = document.querySelector(".witsec-search");
+	const searchMaxChars = (section.dataset.wssearchbodymaxchars !== undefined && !isNaN(section.dataset.wssearchbodymaxchars) ? section.dataset.wssearchbodymaxchars : "9999");
+
+	// Check if we need to cutoff the body text
+	if (entry.body.length > searchMaxChars) {
+		let strStart = 0;
+		let strEnd = searchMaxChars;
+		let shifted = false;
+		let endOfString = false;
+
+		// If we need to shift, shift the body to the point where the first match is found (to not display results that don't have any of the matches mentioned)
+		if (entry.bodyPos >= searchMaxChars) {
+			shifted = true;
+			if (entry.body.length - entry.bodyPos - 15 >= searchMaxChars) {					
+				strStart = entry.bodyPos - 15;
+			}
+			else {
+				strStart = entry.body.length - searchMaxChars;
+				endOfString = true;
+			}
+		}
+		entry.body = (shifted ? "..." : "") + entry.body.substr(strStart, strEnd).trim();
+
+		// If the end of the string was *not* reached, add dots...
+		if (!endOfString) {
+			while (entry.body[entry.body.length-1] === ".")	// Remove trailing dots
+				entry.body = entry.body.slice(0, -1);
+			entry.body = entry.body.trim() + "...";
+		}
+	}
+
+	return entry;
+}
+
+// On local, use dummy data
+function wsDummySearch() {
+	wsQ = "lorem dummy";
+	wsQinternal = "lorem|dummy";
+
+	wsScoreDB = [
+	{
+		anchor: "what-is-lorem-ipsum",
+		body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+		bodyPos: 0,
+		header: "What is Lorem Ipsum?",
+		page: "index.html",
+		score: 5
+	},
+	{
+		anchor: "origins",
+		body: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32.",
+		bodyPos: 29,
+		header: "Where does it come from?",
+		page: "origins.html",
+		score: 3
+	},
+	{
+		anchor: "standard-passage",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		bodyPos: 0,
+		header: "The standard Lorem Ipsum passage, used since the 1500s",
+		page: "passage.html",
+		score: 2
+	}];
+
+	wsDisplayResults();
+}
+
 // When you're ready, let's go
 document.addEventListener("DOMContentLoaded", function(e) { 
+	// Get query parameter
 	wsQ = wsGetParameterByName("q");
 	if (wsQ !== null) {
-		if (window.location.href.startsWith("file://"))
-			alert("Unfortunately the search feature only works online.");
+		if (window.location.href.startsWith("file://")) {
+			alert("Unfortunately, the search feature only works online. We'll display dummy data instead.");
+			wsDummySearch();
+		}
 		else
 			wsReadDatabase();
 	}
+
+	// Remove GDPR block, if present
+	const gdpr = document.querySelector(".witsec-search .gdpr-block");
+	if (gdpr)
+		gdpr.remove();
 });
